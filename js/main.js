f519fda03ebd1613e5e7f9402e12e1f92e853fa0
david cortesi <david.cortesi.097@gmail.com>
2020-11-04 17:58:02 +0100

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,94 @@
+const icons = [
+  {
+    icon: "fas fa-cat" ,
+    name: "cat" ,
+    type: "animal" ,
+  },
+  {
+    icon:  "fad fa-crow",
+    name: "crow" ,
+    type: "animal"  ,
+  },
+  {
+    icon: "fas fa-dog" ,
+    name: "dog" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-dove" ,
+    name: "dove" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-dragon" ,
+    name: "dragon" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-horse" ,
+    name: "horse" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-hippo" ,
+    name: "hippo" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-fish" ,
+    name: "fish" ,
+    type: "animal" ,
+  },
+  {
+    icon: "fas fa-carrot" ,
+    name: "carrot" ,
+    type: "fruit" ,
+  },
+  {
+    icon: "fas fa-apple-alt" ,
+    name: "apple-alt" ,
+    type: "fruit" ,
+  },
+  {
+    icon: "fas fa-lemon" ,
+    name: "lemon" ,
+    type: "fruit" ,
+  },
+  {
+    icon: "fas fa-pepper-hot" ,
+    name: "pepper-hot" ,
+    type: "fruit" ,
+  },
+  {
+    icon: "fas fa-user-astronaut" ,
+    name: "user-astronaut" ,
+    type: "user" ,
+  },
+  {
+    icon: "fas fa-user-graduate" ,
+    name: "user-graduate" ,
+    type: "user" ,
+  },
+  {
+    icon: "fas fa-user-ninja" ,
+    name: "user-ninja" ,
+    type: "user" ,
+  },
+  {
+    icon: "fas fa-user-secret" ,
+    name: "user-secret" ,
+    type: "user" ,
+  },
+];
+icons.forEach((item, i) => {
+  icon = item.icon ;
+  name = item.name;
+  document.getElementsByClassName("principal").append(
+  `
+  <div class="icon">
+    <i class="${icon}"></i>
+    <h5>${name}</h5>
+  </div>
+  ` );
 
+});
